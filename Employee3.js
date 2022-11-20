cf94a30f6125c36720e4aba485915ca194a22eb4
pkr187 <pankajroman187@gmail.com>
2022-11-20 21:57:32 +0530

    [pankaj] Add:RefactorFunction

--- Employee3.js
+++ Employee3.js
@@ -1,25 +1,28 @@
 const IS_PART_TIME = 1;
     const IS_FULL_TIME = 2;
     const RATE_PER_HR=20;
-    
-    let empHrs=0;
-    let empWage=0;
-    let randomInput=Math.floor(Math.random()* 10)%3;
 
+    let randomInput=Math.floor(Math.random()* 10)%3;
     console.log("Randomvalue:"+ randomInput)
-    switch(randomInput)
+    let empWage=0;
+   
+    function getWorkingHrs()
+    {
+        let empHrs=0;
+        switch(randomInput)
     {
         case IS_FULL_TIME:
         empHrs=8;
-        break
+        return empHrs;
 
         case IS_PART_TIME:
             empHrs=4;
-            break
+            return empHrs;
 
         default:
             empWage=0
-            break
+            return empHrs;
+        }
     }
-    empWage=empHrs*RATE_PER_HR;
+    empWage=getWorkingHrs()*RATE_PER_HR;
     console.log("Employee Wage:"+ empWage)
